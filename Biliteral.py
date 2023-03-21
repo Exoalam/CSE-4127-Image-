b575a5aea0e9bf1fb7f25646ac96f93b8e4abfee
Exoalam <nafiul.arpon@gmail.com>
2023-03-21 09:15:30 +0600

    up

--- Biliteral.py
+++ Biliteral.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import math
 def gaussian(k_size, sigma):
-    gas_kernel = np.zeros((k_size, k_size),dtype=np.float64)
+    gas_kernel = np.zeros((k_size, k_size),dtype=np.float32)
     norm = 0
     gas_padding = (gas_kernel.shape[0] - 1) // 2
     for x in range(-gas_padding, gas_padding+1):
@@ -16,7 +16,7 @@ def gaussian(k_size, sigma):
 image = cv2.imread('onion.PNG',cv2.IMREAD_GRAYSCALE)
 
 k_size = 3
-sigma = 1.4
+sigma = 5
 kernel = gaussian(k_size, sigma)
 image_h = image.shape[0]
 image_w = image.shape[1]
@@ -31,7 +31,7 @@ image = cv2.copyMakeBorder(image, padding_y, padding_y, padding_x, padding_x, cv
 output_image_h = image_h + k_size - 1
 output_image_w = image_w + k_size - 1
 
-biliteral_output = np.zeros((output_image_h,output_image_w),dtype=np.float64)
+biliteral_output = np.zeros((output_image_h,output_image_w),dtype=np.float32)
 
 for x in range(padding_x, output_image_h-padding_x):
     for y in range(padding_y, output_image_w-padding_y):
@@ -48,7 +48,7 @@ for x in range(padding_x, output_image_h-padding_x):
                 
         biliteral_output[x,y] = conv/div         
 biliteral_output = cv2.normalize(biliteral_output, None, 0, 1, cv2.NORM_MINMAX)      
-
+print(biliteral_output)
 cv2.imshow('input', image)
 cv2.imshow('biliteral', biliteral_output)
 
